fc979c43dba9438412277f8f969a9b6ff99d5b1b
jacky3314206 <jacky331429@gmail.com>
2020-11-04 19:36:43 +0800

    Elementpractice

---
+++ Elementpractice/app/src/main/java/com/example/elementpractice/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.elementpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.send);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setText("Logout");
+            }
+        });
+    }
+}
